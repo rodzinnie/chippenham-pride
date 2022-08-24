078f32c19764b5635a981b306ffdded0d29c71bd
agiskorska <skorskaga@gmail.com>
2022-08-24 16:44:19 +0100

    Add some styles to header

--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -1,20 +1,23 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+import { Button } from '../../common'
 import styles from './Header.module.css'
 
 function Header() {
   return (
     <header className={styles.root}>
-        <nav>
+        <nav className={styles.navbar}>
             <img src=""  alt="logo"/>
             <div className={styles.title}>Chippenham Pride 2023</div>
             <NavLink to="home">Home</NavLink>
-            <NavLink to="home">Home</NavLink>
-            <NavLink to="home">Home</NavLink>
-            <NavLink to="home">Home</NavLink>
-            <NavLink to="home">Home</NavLink>
+            <NavLink to="home">About</NavLink>
+            <NavLink to="home">Volunteer</NavLink>
+            <NavLink to="home">Support</NavLink>
+            <NavLink to="home">The Team</NavLink>
             <Button text="Contact us" variant="dark" />
         </nav>
-        <section>
+        <section className={styles.section}>
             <h1>Join Proud Chippenham in celebrating its diversity!</h1>
             <p className={styles.date}>17 June 2023</p>
             <p className={styles.start}>Start: 1pm</p>
