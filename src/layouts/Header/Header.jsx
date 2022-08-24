b5a7bcb5d43d028fe033b8bab9e5ba204e5c07db
agiskorska <skorskaga@gmail.com>
2022-08-24 16:17:53 +0100

    Add About and Home page

---
+++ src/layouts/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from './Header.module.css'
+
+function Header() {
+  return (
+    <header className={styles.root}>
+        <nav>
+            
+        </nav>
+    </header>
+  )
+}
+
+export default Header
