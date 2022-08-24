d84bf0669b9fef5bdf9960a54acabfb36e3ac041
agiskorska <skorskaga@gmail.com>
2022-08-24 16:25:51 +0100

    Add header structure

--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -5,8 +5,23 @@ function Header() {
   return (
     <header className={styles.root}>
         <nav>
-            
+            <img src=""  alt="logo"/>
+            <div className={styles.title}>Chippenham Pride 2023</div>
+            <NavLink to="home">Home</NavLink>
+            <NavLink to="home">Home</NavLink>
+            <NavLink to="home">Home</NavLink>
+            <NavLink to="home">Home</NavLink>
+            <NavLink to="home">Home</NavLink>
+            <Button text="Contact us" variant="dark" />
         </nav>
+        <section>
+            <h1>Join Proud Chippenham in celebrating its diversity!</h1>
+            <p className={styles.date}>17 June 2023</p>
+            <p className={styles.start}>Start: 1pm</p>
+            <p className={styles.place}>Monkton Park, Chippenham</p>
+            <Button text="Contact us" variant="light"/>
+            <img src="" alt="arrow down" />
+        </section>
     </header>
   )
 }
--- src/layouts/MainLayout/MainLayout.jsx
+++ src/layouts/MainLayout/MainLayout.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 
 import styles from './MainLayout.module.css'
+import { Header } from '..'
 
 function MainLayout() {
   return (
     <div className={styles.root}>
+        <Header />
         <Outlet />
     </div>
   )
