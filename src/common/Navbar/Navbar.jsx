f502195a893d73c6edf9fdb977bf4d4b8522194d
agiskorska <skorskaga@gmail.com>
2022-08-25 09:43:00 +0100

    Add some logic to the hamburger menu

--- src/common/Navbar/Navbar.jsx
+++ src/common/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import { Sling as Hamburger } from 'hamburger-react'
 
 import { Button } from '..'
@@ -9,20 +9,30 @@ import styles from './Navbar.module.css'
 
 function Navbar() {
     const [isOpen, setOpen] = useState(false)
+    const inputRef = useRef();
 
+    const handleHamburger = (toggled) => {
+        if(toggled) {
+            console.log(inputRef.current)
+            inputRef.current.style.right = 0
+        } else {
+            console.log(inputRef.current)
+            inputRef.current.style.right = '-200%'
+        }
+    }
   return (
     <div className={styles.root}>
             <img src={logo}  alt="logo" className={styles.logo}/>
             <div className={styles.title}>Chippenham Pride 2023</div>
-            <nav>
-                <NavLink to="home">Home</NavLink>
-                <NavLink to="home">About</NavLink>
-                <NavLink to="home">Volunteer</NavLink>
-                <NavLink to="home">Support</NavLink>
-                <NavLink to="home">The Team</NavLink>
+            <nav ref={inputRef} className={styles.nav}>
+                <NavLink className={styles.navlink} to="home">Home</NavLink>
+                <NavLink className={styles.navlink} to="about">About</NavLink>
+                <NavLink className={styles.navlink} to="home">Volunteer</NavLink>
+                <NavLink className={styles.navlink} to="home">Support</NavLink>
+                <NavLink className={styles.navlink} to="home">The Team</NavLink>
                 <Button text="Contact us" variant="dark" />
             </nav>
-            <Hamburger className={styles.hamburger} color="#000000"/>
+            <Hamburger onToggle={handleHamburger} toggled={isOpen} toggle={setOpen} className={styles.hamburger} color="#FFFFFF"/>
     </div>
   )
 }
