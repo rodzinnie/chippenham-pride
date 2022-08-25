5d8d705d05e9a912ad00f0ec5e30e58f2bc62791
agiskorska <skorskaga@gmail.com>
2022-08-25 09:24:45 +0100

    Change Button css

--- src/common/Button/Button.jsx
+++ src/common/Button/Button.jsx
@@ -4,7 +4,7 @@ import styles from './Button.module.css'
 
 function Button({ text, variant}) {
   return (
-    <button>{text}</button>
+    <button className={styles[`${variant}`]}>{text}</button>
   )
 }
 
