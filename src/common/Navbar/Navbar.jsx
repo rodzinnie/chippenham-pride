6949abc43329fccb985f7ba1948c85af6c24bbb5
agiskorska <skorskaga@gmail.com>
2022-08-25 09:54:58 +0100

    Add full functionality to hamburger menu

--- src/common/Navbar/Navbar.jsx
+++ src/common/Navbar/Navbar.jsx
@@ -13,10 +13,8 @@ function Navbar() {
 
     const handleHamburger = (toggled) => {
         if(toggled) {
-            console.log(inputRef.current)
             inputRef.current.style.right = 0
         } else {
-            console.log(inputRef.current)
             inputRef.current.style.right = '-200%'
         }
     }
@@ -25,14 +23,16 @@ function Navbar() {
             <img src={logo}  alt="logo" className={styles.logo}/>
             <div className={styles.title}>Chippenham Pride 2023</div>
             <nav ref={inputRef} className={styles.nav}>
-                <NavLink className={styles.navlink} to="home">Home</NavLink>
-                <NavLink className={styles.navlink} to="about">About</NavLink>
-                <NavLink className={styles.navlink} to="home">Volunteer</NavLink>
-                <NavLink className={styles.navlink} to="home">Support</NavLink>
-                <NavLink className={styles.navlink} to="home">The Team</NavLink>
+                <NavLink className={styles.navlink} to="/">Home</NavLink>
+                <NavLink className={styles.navlink} to="/">About</NavLink>
+                <NavLink className={styles.navlink} to="/">Volunteer</NavLink>
+                <NavLink className={styles.navlink} to="/">Support</NavLink>
+                <NavLink className={styles.navlink} to="/">The Team</NavLink>
                 <Button text="Contact us" variant="dark" />
             </nav>
-            <Hamburger onToggle={handleHamburger} toggled={isOpen} toggle={setOpen} className={styles.hamburger} color="#FFFFFF"/>
+            <div className={styles.hamburger}>
+                <Hamburger onToggle={handleHamburger} toggled={isOpen} toggle={setOpen} color="#FFFFFF"/>
+            </div>
     </div>
   )
 }
