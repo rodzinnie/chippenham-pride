ae4e12367892c8b837cfc7719f5c2bff1ab9aa9f
agiskorska <skorskaga@gmail.com>
2022-08-26 12:36:51 +0100

    Add the footer; Add some stylings

---
+++ src/layouts/Footer/Footer.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import styles from './Footer.module.css'
+import fb from '../../assets/facebook.png'
+import insta from '../../assets/insta.png'
+import tiktok from '../../assets/tiktok.png'
+
+
+function Footer() {
+  return (
+    <footer className={styles.root}>
+      <div className={styles.content}>
+        <nav className={styles.nav}>
+          <NavLink to="about">About</NavLink>
+          <NavLink to="contact">Contact us</NavLink>
+          <NavLink to="fund">Help us fund the PRIDE</NavLink>
+          <NavLink to="tsandcs">T's and C's of the Chippenham Pride 2023</NavLink>
+        </nav>
+        <div className={styles.media}>
+          <a href="facebook.com"><img src={fb} alt="facebook" /></a>
+          <a href="facebook.com"><img src={insta} alt="instagram" /></a>
+          <a href="facebook.com"><img src={tiktok} alt="tiktok" /></a>
+        </div>
+          <div className={styles.registry}>
+            CHIPPENHAM PRIDE CIC <br /> Company number: <br />14287807
+          </div>
+      </div>
+        <p className={styles.rights}>Created &amp; maintained by The Chippenham Pride Team @ 2022</p>
+    </footer>
+  )
+}
+
+export default Footer
