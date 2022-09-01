6225ea28caf94edb20ee6c3291eed0b2a33184de
agiskorska <skorskaga@gmail.com>
2022-09-01 13:49:44 +0100

    Resize the button

--- src/pages/Support/Support.jsx
+++ src/pages/Support/Support.jsx
@@ -3,7 +3,13 @@ import { Button } from '../../common';
 import styles from './Support.module.css'
 import paypal from '../../assets/paypal.png'
 import gofundme from '../../assets/gofundme.png'
+
 function Support() {
+  
+  const handleClick = () => {
+    window.location.assign('https://www.gofundme.com/f/chippenham-pride-2023')
+  }
+
   return (
     <div className={styles.root}>
         <div className={styles.left}>
@@ -11,9 +17,9 @@ function Support() {
             <Button text='Learn more about our spendings' variant='light' />
         </div>
         <div className={styles.right}>
-            <Button text='Donate with PayPal' variant='dark'  />
+            <Button text='Donate with PayPal' variant='dark' className={styles.button}/>
             <img src={paypal} />
-            <Button text='Donate with Go Fund Me' variant='dark'  />
+            <Button text='Donate with Go Fund Me' variant='dark'  className={styles.button} handleClick={handleClick}/>
             <img src={gofundme} />
         </div>
     </div>
