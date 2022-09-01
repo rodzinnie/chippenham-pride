82eb6ae3cff152da79461063d0e9e983dd05faa5
agiskorska <skorskaga@gmail.com>
2022-09-01 12:22:05 +0100

    Continue adding styles to support page

--- src/pages/Support/Support.jsx
+++ src/pages/Support/Support.jsx
@@ -1,18 +1,20 @@
 import React from 'react'
 import { Button } from '../../common';
 import styles from './Support.module.css'
+import paypal from '../../assets/paypal.png'
+import gofundme from '../../assets/gofundme.png'
 function Support() {
   return (
     <div className={styles.root}>
         <div className={styles.left}>
             <div className={styles.shoutout}>Help us support the pride!</div>
-            <Button text='Learn more about our spendings' />
+            <Button text='Learn more about our spendings' variant='light' />
         </div>
         <div className={styles.right}>
-            <Button text='Donate with:' />
-            <img></img>
-            <Button text='Donate with:' />
-            <img></img>
+            <Button text='Donate with PayPal' variant='dark'  />
+            <img src={paypal} />
+            <Button text='Donate with Go Fund Me' variant='dark'  />
+            <img src={gofundme} />
         </div>
     </div>
   )
