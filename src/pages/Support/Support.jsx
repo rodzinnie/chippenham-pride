8687ce881e3056fef7a7f5f582c544481daefb66
agiskorska <skorskaga@gmail.com>
2022-10-06 06:19:09 +0100

    Update Support website

--- src/pages/Support/Support.jsx
+++ src/pages/Support/Support.jsx
@@ -3,6 +3,9 @@ import { Button } from '../../common';
 import styles from './Support.module.css'
 import paypal from '../../assets/paypal.png'
 import gofundme from '../../assets/gofundme.png'
+import sponsorship from '../../assets/Sponsorship1.pdf'
+import sponsorshipSmallBiz from '../../assets/SponsorshipSmallBiz.pdf'
+import sponsorshipCorporate from '../../assets/SponsorshipCorporates.pdf'
 
 function Support() {
   const more = useRef();
@@ -20,10 +23,23 @@ function Support() {
 
   return (
     <div className={styles.root}>
+        <article className={styles.sponsorPacks}>
+          <h1 className={styles.h1}>Learn about our Sponsorship Packages</h1>
+          <a href={sponsorship} className={styles.button} download>
+            <Button text='Download a general Sponsorship pack'></Button>
+          </a>
+          <a href={sponsorshipSmallBiz} className={styles.button} download>
+            <Button text='Download Sponsorship pack for Small Businesses'></Button>
+          </a>
+          <a href={sponsorshipCorporate} className={styles.button} download>
+            <Button text='Download Sponsorship pack for Corporates'></Button>
+          </a>
+        </article>
+
         <div className={styles.left}>
             <div className={styles.shoutout}>Help us support the pride!</div>
             <Button text='Learn more about our spendings' variant='light' handleClick={showMore} />
-          <div ref={more} className={styles.more}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime omnis recusandae rem neque quis commodi molestiae. Necessitatibus in deserunt minus accusamus veritatis omnis ullam porro rem at aut nam, tenetur recusandae magni amet ab nulla unde sunt quis ut inventore? Atque reprehenderit porro cupiditate earum repellat, saepe numquam iure quam!</div>
+          <div ref={more} className={styles.more}>Chippenham Pride is a pro-bono charity. We have a policy of total transparency. If you want to know more about our spendings, please contact Bryan via our contact form.</div>
         </div>
         <div className={styles.right}>
             <Button text='Donate with PayPal' variant='dark' className={styles.button} handleClick={() => console.log('elo')}/>
