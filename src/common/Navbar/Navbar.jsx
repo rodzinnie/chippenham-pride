04bb395495d8a7c903d4918ebaa2546589de9475
agiskorska <skorskaga@gmail.com>
2022-10-19 11:39:44 +0100

    Fix donate button in navbar

--- src/common/Navbar/Navbar.jsx
+++ src/common/Navbar/Navbar.jsx
@@ -73,7 +73,6 @@ function Navbar() {
                 <form className={styles.ppform} action="https://www.paypal.com/donate" method="post" target="_top">
                     <input type="hidden" name="campaign_id" value="R7BQRSQPLBM3A" />
                     <input className={styles.donate} type="image" src="https://www.paypalobjects.com/en_GB/i/btn/btn_donate_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
-                    {/* <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1" /> */}
                 </form>
             </nav>
             <div className={styles.hamburger}>
