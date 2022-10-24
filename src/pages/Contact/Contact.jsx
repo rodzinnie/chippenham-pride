c4943e0e4749116766edfc36ed119765e2736d4c
agiskorska <skorskaga@gmail.com>
2022-10-24 08:01:04 +0100

    Add the dot

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -5,7 +5,7 @@ import styles from './Contact.module.css'
 function Contact() {
   return (
     <ArticleSection className={styles.root} header='Contact us!'>
-      <p>We'd love to hear from you whether it is about helping us, supporting us financially or just to tell us your story</p>
+      <p>We'd love to hear from you whether it is about helping us, supporting us financially or just to tell us your story.</p>
       <Form />
     </ArticleSection>
   )
