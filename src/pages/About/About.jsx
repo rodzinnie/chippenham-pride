30803a75d3b3a5c5024c986f2e7171870682f5a8
agiskorska <skorskaga@gmail.com>
2022-10-25 05:56:06 +0100

    Update About page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -7,7 +7,9 @@ function About() {
 
       <h3 className={styles.subheader}><i>Alone we can do so little; together we can do so much.</i> - Hellen Keller</h3>
       <p>In 2023 in The UK we are lucky enough we mostly don't have to fight for our recognition as equal citizens, however, we still have some work to do. There are still young people afraid to come out to their parents, people are still having shameful feelings about being 'Queer'. Still, first thing we do going abroad is checking if we can hold our loved ones by their hand, not having to worry about going to prison or being beat up.</p>
-      <br /> 
+      <p>We are set up to provide Chippenham and the 18 surrounding villages with not only a pride event that brings the fun and vibrancy into the town centre but to also celebrate! Support, educate and represent all members of the LGBTQIA+ community</p>
+      <p>Our biggest focus after the entertaining is the Mental and physical well being. Did you know that 50% of LGBTQ teens (ages 13–17) seriously considered attempting suicide in the past year. And 18 percent actually made a suicide attempt. That’s more than twice the rate of suicide attempts amongst all teens, which is 9% - still not an acceptable rate! </p>
+      <p>Our well-being, information and safe space area are inviting local and national charities to come along with information and support for not only this but all areas of mental, physical and sexual health. PLEASE KNOW YOU ARE NOT ALONE 🏳️‍🌈❤️</p>
       <p>With the Chippenham Pride, we are hoping to achieve more equality across our town. This beautiful market town deserves being enjoyed by all, and we want everyone to know we support them!</p>
 
     </ArticleSection>
