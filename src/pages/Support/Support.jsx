3ae7ca803059ec8c7079ab846416f50b9a5bd683
agiskorska <skorskaga@gmail.com>
2022-11-02 11:58:04 +0000

    Take paypal off the website

--- src/pages/Support/Support.jsx
+++ src/pages/Support/Support.jsx
@@ -2,7 +2,6 @@ import React, { useRef } from 'react'
 import { ArticleSection, Button } from '../../common';
 import styles from './Support.module.css'
 import gofundme from '../../assets/gofundme.png'
-import paypal from '../../assets/paypal.png'
 
 
 function Support() {
@@ -24,27 +23,18 @@ function Support() {
   }
 
   return (
-    <div className={styles.root}>
+    <ArticleSection className={styles.root} header='Help us support the pride!'>
         <div className={styles.left}>
-            <div className={styles.shoutout}>Help us support the pride!</div>
+            <div className={styles.shoutout}></div>
+            <p className={styles.p}>Our cause is simple: promote equality in our beautiful town. Without YOUR support, we wouldn't be able to make it!</p>
             <Button text='Learn more about our spendings' variant='light' handleClick={showMore} />
           <div ref={more} className={styles.more}>Chippenham Pride is a pro-bono charity. We have a policy of total transparency. If you want to know more about our spendings, please contact Bryan via our contact form.</div>
         </div>
         <div className={styles.right}>
-            {/* <Button text='Donate with PayPal' variant='dark' className={styles.button} handleClick={handleClickPP}/> */}
-            <iframe
-              className={styles.donatepp}
-              src="https://www.paypal.com/giving/campaigns?campaign_id=R7BQRSQPLBM3A"
-              title="PayPal donate campaign card"
-              frameborder="0"
-              width='400'
-              height='425'
-              background='transparent'
-              scrolling="no"></iframe>
             <Button text='Donate with Go Fund Me' variant='dark'  className={styles.button} handleClick={handleClick}/>
             <img className={styles.img} src={gofundme} />
         </div>
-    </div>
+    </ArticleSection>
   )
 }
 
