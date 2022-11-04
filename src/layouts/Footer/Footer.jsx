605e046d1cc240ffa1343364d656ea46b1220148
agiskorska <skorskaga@gmail.com>
2022-11-04 16:27:32 +0000

    Add dark obsession as sponsors

--- src/layouts/Footer/Footer.jsx
+++ src/layouts/Footer/Footer.jsx
@@ -8,6 +8,7 @@ import pppride from '../../assets/prideqr.png'
 import titc from '../../assets/titc.png'
 import winemo from '../../assets/winemonkey.png'
 import frankie from '../../assets/frankielane.png'
+import darkObs from '../../assets/darkobsession.jpg'
 
 
 function Footer() {
@@ -16,6 +17,7 @@ function Footer() {
       <h2>Sponsors</h2>
       <section className={styles.sponsors}>
       <a href='https://www.transinthecity.co.uk/'><img src={titc} alt="trans in the city" className={styles.sponsorImg} /></a>
+      <a href=' https://www.winemonkey.co.uk'><img src={darkObs} alt="dark" className={styles.sponsorImg} /> </a>
       <a href=' https://www.winemonkey.co.uk'><img src={winemo} alt="wine monkey logo" className={styles.sponsorImg} /> </a>
       <a href='https://frankielane.co.uk/'><img src={frankie} alt="frankie lane logo" className={styles.sponsorImg} /> </a>
       </section>
