5b755ea2ddca352b95df808746171d544c502c5a
agiskorska <skorskaga@gmail.com>
2022-11-07 11:42:38 +0000

    Change text

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -7,7 +7,7 @@ function About() {
       <ArticleSection header='Mission Statement'>
         <p>We're committed to celebrating our LGBTQIA+ Community. We aim to work toward a future without discrimination where all people have equal rights.
             We aim to host LGBTQIA+ Pride events that inspire, educate, commemorate and celebrate our diverse community within Chippenham and the surrounding areas. We want EVERYONE to know they are not alone! 
-            An Inspirational family event that helps support allies as well as the direct community with stalls for mental health, physical health and all other LGBTQIA+ needs. Bringing the fun, acceptance and vibrancy Into our beautiful market town and local businesses footfall to. 
+            An Inspirational family event that helps support allies as well as the direct community with stalls for mental health, physical health and all other LGBTQIA+ needs. Bringing the fun, acceptance and vibrancy Into our beautiful market town. 
         </p>
         <p><strong>Education: </strong>We aim to deliver informative and valuable education to the LGBTQIA+
           Community AND  the wider community through inviting local as well as national organisations/ charities to host stalls and give information freely to those who
