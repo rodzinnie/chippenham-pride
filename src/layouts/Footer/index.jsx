54a5718ecfe1b7abd0ab223b733546e4393cc3a5
agiskorska <skorskaga@gmail.com>
2022-11-07 11:36:31 +0000

    Change donate button

--- src/layouts/Footer/index.jsx
+++ src/layouts/Footer/index.jsx
@@ -9,6 +9,7 @@ import titc from '../../assets/titc.png'
 import winemo from '../../assets/winemonkey.png'
 import frankie from '../../assets/frankielane.png'
 import darkObs from '../../assets/darkobsession.jpg'
+import { GoFundMe } from '../../common';
 
 
 function Footer() {
@@ -39,7 +40,7 @@ function Footer() {
         <div className={styles.registry}>
           CHIPPENHAM PRIDE CIC <br /> Company number: <br />14287807
           
-        <div className={styles.qr}><div className={styles.donate}><strong>Donate</strong></div><img src={pppride} /></div>
+        <GoFundMe noImage={true}/>
         </div>
       </div>
         <p className={styles.rights}>Created &amp; maintained by The Chippenham Pride Team 2022 Ⓒ</p>
