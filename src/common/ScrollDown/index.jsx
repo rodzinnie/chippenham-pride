36a78fa2e68e435c10695f65ae8750de7b1443ab
agiskorska <skorskaga@gmail.com>
2022-11-21 21:59:20 +0000

    fix scrolldown

--- src/common/ScrollDown/index.jsx
+++ src/common/ScrollDown/index.jsx
@@ -1,14 +1,11 @@
-import { useEffect } from "react";
+import { useEffect , useState} from "react";
 import { useLocation } from "react-router-dom";
 
 export default function ScrollDown() {
   const { pathname } = useLocation();
 
   useEffect(() => {
-    if(pathname != '/') {
-        setTimeout(() => {window.scrollTo({ top: 500, behavior: 'smooth' });}, 100)
-        
-    }
+    setTimeout(() => {window.scrollTo({ top: 850, behavior: 'smooth' });}, 100)       
   }, [pathname]);
 
   return null;
