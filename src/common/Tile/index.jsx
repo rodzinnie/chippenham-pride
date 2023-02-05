78158dc19ed88c0c79f05fccdae59fbd2cfb6df1
agiskorska <skorskaga@gmail.com>
2023-02-05 23:39:38 +0000

    Add tiles

---
+++ src/common/Tile/index.jsx
@@ -0,0 +1,14 @@
+import {Carousel} from '../'
+import styles from './index.module.css'
+
+function Tile({data}) {
+  return (
+    <div className={styles.root}>
+      <div className={styles.carousel}><Carousel slides={} timer='3000'/></div>
+      <h2 className={styles.heading}>{data.title}</h2>
+      <p className={styles.para}>{data.text}</p>
+    </div>
+  )
+}
+
+export default Tile
