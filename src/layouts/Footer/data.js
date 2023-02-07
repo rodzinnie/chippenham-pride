ccedcc23eb466fa9037925c09c8da3bda2c555fc
Aggie Skorska <57585879+agiskorska@users.noreply.github.com>
2023-02-07 13:29:33 +0000

    Update CMC

--- src/layouts/Footer/data.js
+++ src/layouts/Footer/data.js
@@ -1,4 +1,4 @@
 export const data = [
   {url: 'https://www.transinthecity.co.uk/', src: 'https://i.postimg.cc/rsbhS69L/Tit-C-Logo-Google.jpg', alt: 'trans in the city' },
-  {url: 'https://www.chippenhammotor.co.uk/', src: 'https://i.postimg.cc/XNtPdNpn/motor.png', alt: 'chippenham motor company' },
+  {url: 'https://www.chippenhammotor.co.uk?utm_source=pridewebsite&utm_medium=pridewebsite&utm_campaign=pridesponsorship&utm_content=prideheadlinesponsorarea', src: 'https://i.postimg.cc/qBtKZ0Pv/Pride-Website-Logo.png', alt: 'chippenham motor company' },
 ]
