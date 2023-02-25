cb75a29cfa67c4d100caf2edb65d0a4df19e0371
agiskorska <skorskaga@gmail.com>
2023-02-25 20:59:37 +0000

    Add new sponsors

--- src/pages/Support/data.js
+++ src/pages/Support/data.js
@@ -1,11 +1,18 @@
-export const data = [
+export const sponsors = [
   {url: 'https://frankielane.co.uk/', src: 'https://i.postimg.cc/ZKKs2cQN/frankielane.png', alt: 'frankie lane logo' },
   {url: 'https://www.thedancespot.co.uk/', src: 'https://i.postimg.cc/Mpgsc8p0/tds.png', alt: 'The dance spot logo' },
   {url: 'https://www.manderduffill.com/', src: 'https://i.postimg.cc/HkrZsLds/md.png', alt: 'mander duffill logo' },
   {url: 'https://www.gewefa.co.uk/', src: 'https://i.postimg.cc/CLPrg8yQ/GEWEFA.png', alt: 'GEWEFA logo' },
   {url: 'https://www.facebook.com/theflyingmonkpub/', src: 'https://i.postimg.cc/FFxnTqTm/flyingmonk.jpg', alt: 'The Flying Monk logo' },
   {url: ' https://www.winemonkey.co.uk', src: 'https://i.postimg.cc/Pxj24zZP/winemonkey.png', alt: 'wine monkey logo' },
-  {url: 'https://www.transinthecity.co.uk/', src: 'https://i.postimg.cc/4dQwF7bw/titc.png', alt: 'trans in the city' },
+  {url: 'https://www.transinthecity.co.uk/', src: 'https://i.postimg.cc/rsbhS69L/Tit-C-Logo-Google.jpg', alt: 'trans in the city' },
   {url: 'https://www.bostonburger.co.uk/', src: 'https://i.postimg.cc/0NmVDHzT/bostonburger.jpg  ', alt: 'boston burger logo' },
+  {url: 'https://www.chippenhammotor.co.uk/?utm_source=pridewebsite&utm_medium=pridewebsite&utm_campaign=pridesponsorship&utm_content=prideheadlinesponsorarea', src: 'https://i.postimg.cc/qBtKZ0Pv/Pride-Website-Logo.png', alt: 'boston burger logo' },
   {url: 'https://instagram.com/darkobsessiontattoo?igshid=YmMyMTA2M2Y=', src: 'https://i.postimg.cc/j2qvVxMT/darkobsession.jpg', alt: 'dark'}
+]
+
+export const supporters = [
+  {url: 'https://https://sticky.to/', src: 'https://i.postimg.cc/kgFdFGxh/stickylogo.png', alt: 'Sticky' },
+  {url: 'https://chippenhamhospitalradio.com/', src: 'https://chippenhamhospitalradio.com/sites/default/files/logo/CHRlogo_main.gif', alt: 'chippenham hospital radio'}
+
 ]
