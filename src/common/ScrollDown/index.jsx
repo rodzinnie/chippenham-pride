efb9550a8d50e3ded1896dfbf54f7f3096e78802
agiskorska <skorskaga@gmail.com>
2023-03-07 23:58:05 +0000

    Remove scroll on home path

--- src/common/ScrollDown/index.jsx
+++ src/common/ScrollDown/index.jsx
@@ -5,7 +5,7 @@ export default function ScrollDown() {
   const { pathname } = useLocation();
 
   useEffect(() => {
-    setTimeout(() => {window.scrollTo({ top: 850, behavior: 'smooth' });}, 100)       
+    pathname != '/' ? setTimeout(() => {window.scrollTo({ top: 850, behavior: 'smooth' });}, 100) : null
   }, [pathname]);
 
   return null;
