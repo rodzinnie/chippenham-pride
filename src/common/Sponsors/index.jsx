8525fb97490d541ddd18cf500af95b3f007fe8f8
agiskorska <skorskaga@gmail.com>
2023-03-10 20:49:10 +0000

    Fix css

--- src/common/Sponsors/index.jsx
+++ src/common/Sponsors/index.jsx
@@ -15,12 +15,12 @@ function Sponsors({data, header}) {
       <span className={styles.goldTitle}>Other Sponsors</span>
       <div className={styles.sponsors}>
       {gold.map((e, i) => {
-        return(<a target='_blank' href={e.url} key={i}><img src={e.src} alt={e.alt} className={styles.sponsorImgGold} /></a>)
+        return(<a target='_blank' href={e.url} key={i} className={styles.wrapper}><img src={e.src} alt={e.alt} className={styles.sponsorImgGold} /></a>)
       })}
       </div>
       <div className={styles.sponsors}>
       {other.map((e, i) => {
-        return(<a target='_blank' href={e.url} key={i}><img src={e.src} alt={e.alt} className={styles.sponsorImgGold} /></a>)
+        return(<a target='_blank' href={e.url} key={i} className={styles.wrapper}><img src={e.src} alt={e.alt} className={styles.sponsorImgGold} /></a>)
       })}
       </div>
     </ArticleSection>
