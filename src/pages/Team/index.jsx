9b5648e7b4a3c6e1c5d26434e2ae8fd7ecbaf44a
agiskorska <skorskaga@gmail.com>
2023-03-12 21:08:53 +0000

    Sublte changes

--- src/pages/Team/index.jsx
+++ src/pages/Team/index.jsx
@@ -14,8 +14,8 @@ function Team() {
       <ArticleSection header={'Meet our Team'} />
       <div className={styles.root}>
         <PersonCard name={'Penny Richman'} role={'chair'} email={'penny@chippenhampride.com'} src={penny}/>
-        <PersonCard name={'Bobbi Pickard'} role={'sponsorship'} email={'sponsorship@chippenhampride.com'} src={bobby}/>
-        <PersonCard name={'Stuart Rolfe'} role={'entertainment'} email={'entertainment@chippenhampride.com'} src={stu}/>
+        <PersonCard name={'Bobbi Pickard'} role={'sponsorship'} email={'sponsorship@chippenhampride.com'} src={'https://www.transinthecity.co.uk/uploads/1/2/4/1/12414535/bobbiwebsite_orig.jpg'}/>
+        <PersonCard name={'Stuart Rolfe'} role={'entertainment'} email={'entertainment@chippenhampride.com'} src={'https://i.scdn.co/image/ab6761610000e5eb2f34440ca30dd0c4e8f6b588'}/>
         <PersonCard name={'Bryan Houde'} role={'treasurer'} email={'accounts@chippenhampride.com'} src={bryan}/>
       </div>
       <h2 className={styles.subheader}>And others</h2>
