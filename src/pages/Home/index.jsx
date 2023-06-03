e030f36e37e3dc755143dd585ab48c95e15e8d00
Aggie Skorska <aggie.skorska@kingfisher.com>
2023-06-03 21:37:54 +0100

    Swap IMG for PDF

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,16 +1,13 @@
 import React from 'react'
 
 import styles from './index.module.css'
-import { Form, ArticleSection, Button, GoFundMe, Highlights, MerchLink } from '../../common'
-import prog from '../../assets/PrideProgramme.jpg'
+import { ArticleSection, Button, GoFundMe, Highlights, MerchLink } from '../../common'
+import prog from '../../assets/pride_programme.pdf'
 import {data, other} from './data'
 
 function Home() {
   const handleClick = (e) => {
     window.open('https://sticky.to/go/flow/a90fbb2a-6695-4dc9-882f-649eaf617844', '_blank').focus()
-  }
-  const openPlan = (e) => {
-    window.open('https://i.postimg.cc/Bbyv9jCM/IMG-1677.jpg', '_blank').focus()
   }
 
   return (
