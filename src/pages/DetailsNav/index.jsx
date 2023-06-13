f4c77b0c6db30237bed10dc467b6730b71b79d35
Aggie Skorska <aggie.skorska@kingfisher.com>
2023-06-13 10:46:23 +0100

    Add programme and map in event details section

--- src/pages/DetailsNav/index.jsx
+++ src/pages/DetailsNav/index.jsx
@@ -1,19 +1,39 @@
-import { useSelector } from 'react-redux'
-
-import {ArticleSection, LineUp} from '../../common'
-import styles from './index.module.css'
-import {data} from '../../data'
+import { useSelector } from "react-redux";
+import prog from '../../assets/pride_programme.pdf' 
+import map from '../../assets/Map.pdf' 
+import { ArticleSection, LineUp } from "../../common";
+import styles from "./index.module.css";
+import { data } from "../../data";
 const DetailsNav = () => {
-  const lineup = useSelector((state) => state.data.lineup)
+  const lineup = useSelector((state) => state.data.lineup);
 
   return (
     <>
-      <LineUp lineup={lineup}/>
-      <ArticleSection header={'the event'}>
-        <div className={styles.list} dangerouslySetInnerHTML={{__html: data.eventDetailTexts.list}}></div>
+    <ArticleSection header={'the Programme'}>
+      <img
+        src="https://i.postimg.cc/Bbyv9jCM/IMG-1677.jpg"
+        alt="Pride programme"
+        style={{ display: "block" }}
+      />
+      <p style={{ width: "fit-content", margin: "40px auto" }}>
+        <a href={prog} download>
+          Download Programme
+        </a>
+      </p>
+      <p style={{ width: "fit-content", margin: "40px auto" }}>
+        <a href={map} download>
+          Download Map
+        </a>
+      </p>
+      </ArticleSection>
+      <ArticleSection header={"the event"}>
+        <div
+          className={styles.list}
+          dangerouslySetInnerHTML={{ __html: data.eventDetailTexts.list }}
+        ></div>
       </ArticleSection>
     </>
-  )
-}
+  );
+};
 
-export default DetailsNav
+export default DetailsNav;
