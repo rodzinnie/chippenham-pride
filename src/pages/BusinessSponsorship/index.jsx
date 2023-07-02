08a33c6c1c7b730ce7616a5753db0a4049640c5a
Aggie Skorska <aggie.skorska@kingfisher.com>
2023-07-02 22:03:33 +0100

    Update sponsors

--- src/pages/BusinessSponsorship/index.jsx
+++ src/pages/BusinessSponsorship/index.jsx
@@ -1,31 +1,36 @@
-import React from 'react'
+import React from "react";
 
-import {Button, ArticleSection} from '../../common'
-import styles from './index.module.css'
-import sponsorship from '../../assets/Sponsorship1.pdf'
-import sponsorshipSmallBiz from '../../assets/SponsorshipSmallBiz.pdf'
-import sponsorshipCorporate from '../../assets/SponsorshipCorporates.pdf'
+import { Button, ArticleSection } from "../../common";
+import styles from "./index.module.css";
+import sponsorship from "../../assets/Sponsorship1.jpeg";
+import sponsorshiptnc from "../../assets/Sponsorshiptnc.jpeg";
+import headline from "../../assets/headlinesponsorship.jpeg";
+import other from "../../assets/othersponsorship.jpeg";
 
 function BusinessSponsorship() {
   return (
     <div className={styles.root}>
       <article className={styles.sponsorPacks}>
-      <ArticleSection header='Learn about our Sponsorship Packages' />
-      <a href={sponsorship} className={styles.button} download>
-        <h3 className={styles.h3}>General Sponsorship Pack</h3>
-        <Button variant='dark' text='Download'></Button>
-      </a>
-      <a href={sponsorshipSmallBiz} className={styles.button} download>
-        <h3 className={styles.h3}>Sponsorship pack for Small Businesses</h3>
-        <Button variant='dark' text='Download'></Button>
-      </a>
-      <a href={sponsorshipCorporate} className={styles.button} download>
-        <h3 className={styles.h3}>Sponsorship pack for Corporates</h3>
-        <Button variant='dark' text='Download'></Button>
-      </a>
-    </article>
-  </div>
-  )
+        <ArticleSection header="Learn about our Sponsorship Packages" />
+        <a href={sponsorship} className={styles.button} download>
+          <h3 className={styles.h3}>Sponsorship Agreement</h3>
+          <Button variant="dark" text="Download"></Button>
+        </a>
+        <a href={sponsorshiptnc} className={styles.button} download>
+          <h3 className={styles.h3}>T's and C's</h3>
+          <Button variant="dark" text="Download"></Button>
+        </a>
+        <a href={headline} className={styles.button} download>
+          <h3 className={styles.h3}>Headline sponsors packages</h3>
+          <Button variant="dark" text="Download"></Button>
+        </a>
+        <a href={other} className={styles.button} download>
+          <h3 className={styles.h3}>Other sponsor packages</h3>
+          <Button variant="dark" text="Download"></Button>
+        </a>
+      </article>
+    </div>
+  );
 }
 
-export default BusinessSponsorship
+export default BusinessSponsorship;
